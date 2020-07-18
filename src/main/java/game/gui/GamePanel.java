88f5809cef345c2827f961843747600eea091139
Tomasz Sosnowski <tsosnowski666@gmail.com>
2020-07-18 13:36:01 +0200

    added TicTacToe implementation diagram

--- src/main/java/game/gui/GamePanel.java
+++ src/main/java/game/gui/GamePanel.java
@@ -2,11 +2,14 @@ package game.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements MouseListener {
 
     public GamePanel(){
         super();
+        addMouseListener(this);
     }
 
     @Override
@@ -21,5 +24,38 @@ public class GamePanel extends JPanel {
 
         g2d.drawLine(0, 180, 600, 180);
         g2d.drawLine(0,360, 600, 360);
+
+        int widthPoint  = 300;
+        int heightPoint = 450;
+        g2d.drawOval(widthPoint - (80 / 2), heightPoint - (80 / 2), 80, 80);
+
+        widthPoint = 500;
+        heightPoint = 450;
+        g2d.drawOval(widthPoint - (80 / 2), heightPoint - (80 / 2), 80, 80);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println(e.getX() + " " + e.getY());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
